a1c896eca3e918a921dc6f8a052f843ca581658a
Mazweedukas <mazvydas4444@gmail.com>
2026-06-11 18:20:32 +0300

    add task service

---
+++ TaskManagerApi/Services/Interfaces/ITaskService.cs
@@ -0,0 +1,13 @@
+﻿using TaskManagerApi.DTOs.Tasks;
+
+namespace TaskManagerApi.Services.Interfaces
+{
+    public interface ITaskService
+    {
+        Task<IEnumerable<Models.Task>> GetAllAsync();
+        Task<int> CreateAsync(CreateTaskDto task);
+        Task<Models.Task?> GetByIdAsync(int id);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> UpdateAsync(int id, UpdateTaskDto task);
+    }
+}
---
+++ TaskManagerApi/Services/TaskService.cs
@@ -0,0 +1,34 @@
+﻿using TaskManagerApi.DTOs.Tasks;
+using TaskManagerApi.Repositories.Interfaces;
+using TaskManagerApi.Services.Interfaces;
+
+namespace TaskManagerApi.Services;
+
+public class TaskService : ITaskService
+{
+    private readonly ITaskRepository _taskRepository;
+    public TaskService(ITaskRepository taskRepository)
+    {
+        _taskRepository = taskRepository;
+    }
+    public async Task<IEnumerable<Models.Task>> GetAllAsync()
+    {
+        return await _taskRepository.GetAllAsync();
+    }
+    public async Task<int> CreateAsync(CreateTaskDto task)
+    {
+        return await _taskRepository.CreateAsync(task);
+    }
+    public async Task<Models.Task?> GetByIdAsync(int id)
+    {
+        return await _taskRepository.GetByIdAsync(id);
+    }
+    public async Task<bool> UpdateAsync(int id, UpdateTaskDto task)
+    {
+        return await _taskRepository.UpdateAsync(id, task);
+    }
+    public async Task<bool> DeleteAsync(int id)
+    {
+        return await _taskRepository.DeleteAsync(id);
+    }
+}
